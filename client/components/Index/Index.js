50f16bb1d37916c26e677d7b8c806b32290201e8
Martin Contreras <dev@martinccontreras.com>
2017-04-22 23:31:18 -0400

    add splash page image and footer updates

--- client/components/Index/Index.js
+++ client/components/Index/Index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Header from '../Header';
 import Navigation from '../Navigation';
 import Footer from '../Footer';
+import Splash from '../Splash';
 
 const IndexComponent = () => (
   <section>
     <Header />
     <Navigation />
+    <Splash />
     <Footer />
   </section>
 );
---
+++ client/components/Splash/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './styles/index.scss';
+
+const Splash = () => (
+	<section className="splash">
+		<img src={"http://placehold.it/800x600?text=splash+page+image+here"} />
+	</section>
+);
+export default Splash;
