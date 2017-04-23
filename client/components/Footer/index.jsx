2fb984a01596d9edf5c49cbc4b4bfc656f41dd3f
Martin Contreras <dev@martinccontreras.com>
2017-04-22 23:23:33 -0400

    add footer updates

--- client/components/Footer/index.jsx
+++ client/components/Footer/index.jsx
@@ -3,6 +3,22 @@ import './styles/index.scss';
 
 const Footer = () => (
   <footer>
+  	<ul>
+  		<li>Contact Us</li>
+  		<li>
+  			<div className="icons">
+  				<i className="fa fa-facebook" aria-hidden="true"></i>
+  				<i className="fa fa-twitter" aria-hidden="true"></i>
+  				<i className="fa fa-instagram" aria-hidden="true"></i>
+  				<i className="fa fa-spotify" aria-hidden="true"></i>
+			</div>
+  		</li>
+  		<li
+  			className="made"
+  		>
+  			Made with <i className="fa fa-heart" aria-hidden="true"></i> by <a href="//www.martinccontreras.com">MCC</a>
+  		</li>
+  	</ul>
   </footer>
 );
 export default Footer;
